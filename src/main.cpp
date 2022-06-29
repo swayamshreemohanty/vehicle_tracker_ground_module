e3dc06bcef641caac2a2206de435952a4a0fa79a
Ayush-Routray <108125338+Ayush-Routray@users.noreply.github.com>
2022-06-28 22:32:10 -0700

    sms decoder added

--- src/main.cpp
+++ src/main.cpp
@@ -6,128 +6,161 @@ static const uint32_t GPSBaud = 9600;
 TinyGPSPlus gps;
 SoftwareSerial gpsSerial(5, 4); //RX, TX
 SoftwareSerial gsmSerial(9, 10); //RX, TX
-String stringVal = "";
-String number = "+917609934272"; //-> change with your number
-int temp=0;
-char gsmState ='i';
-
-void reset_gsm_state(){
-  gsmState ='i';
-}
+String number = "+917008865793"; //-> change with your number
+boolean allowGPSSearching=1;
+String stringVal="";
 
 void init_receive_sms(){
+  gsmSerial.listen();
   Serial.println("Init receive");
   gsmSerial.println("AT+CMGF=1"); // Configuring TEXT mode
   delay(500);
   gsmSerial.println("AT+CNMI=2,2,0,0,0");
   delay(400);
-  reset_gsm_state();
+  return;
+}
+
+void init_sms(){
+  Serial.println("Init Send");
+  gsmSerial.println("AT+CMGF=1");
+  delay(400);
+  gsmSerial.println("AT+CMGS=\""+number+"\"");
+  delay(400);
+  return;
 }
 
 void setup() {
   Serial.begin(9600);
   gpsSerial.begin(GPSBaud);
   gsmSerial.begin(GPSBaud);
-  Serial.print("Vehicle Tracking");
+  Serial.println("Vehicle Tracking");
   delay(2000);
   init_receive_sms();
-  temp=0;
 }
 
 
-void init_sms(){
-  gsmSerial.println("AT+CMGF=1");
-  delay(400);
-  gsmSerial.println("AT+CMGS=\""+number+"\"");
-  delay(400);
-  reset_gsm_state();
-}
+
 //
 void send_data(String message){
   gsmSerial.print(message);
   delay(200);
+  return;
 }
-void send_sms(){
-  gsmSerial.write(26);
-  delay(1000);
+
+void send_sms(String message){
+  init_sms();
+  gsmSerial.print(message);
+  gsmSerial.println((char)26);// ASCII code of CTRL+Z for saying the end of sms to  the module
+  delay(500);
+  init_receive_sms();
+  return;
+}
+
+
+void tracking()
+{
+  while (allowGPSSearching)
+  {
+    gpsSerial.listen();
+   // Serial.println("Searching for GPS Data");
+   while (gpsSerial.available()>0)
+    {
+     Serial.println();
+     Serial.println(gpsSerial.read());
+     Serial.println();
+     gps.encode(gpsSerial.read());
+     Serial.println(gps.location.lat());
+     if (gps.location.isUpdated())
+     {
+       init_sms();
+       gsmSerial.print("your vehicle lucation is:");
+       gsmSerial.print("latitude: ");
+       gsmSerial.print(gps.location.lat(), 6);
+       gsmSerial.print(",longitude: ");
+       gsmSerial.println(gps.location.lng(), 6);
+       //Send SMS
+       gsmSerial.write(26);
+       // gsmSerial.println((char)26);// ASCII code of CTRL+Z for saying the end of sms to  the module
+       delay(100);
+       //
+       init_receive_sms();
+     }
+      allowGPSSearching=0;
+     break;
+    }
+  }
+  return;
+}
+
+//
+bool isContain(String payload, String find){
+  int payloadLength=payload.length();
+  int findPayloadLength=find.length();
+  if (findPayloadLength>payloadLength)
+  {
+    return false;
+  }
+  else
+  {
+    int index=0;
+    int found=0;
+    while (index<payloadLength)
+    {
+      if (find[found] == payload[index])
+      {
+        found++;
+        if (found==findPayloadLength)
+        {
+          return true;
+        }
+      }
+      else
+      {
+        found=0;
+      }
+      index++;
+    }
+    return false;
+  }
 }
 //
+
+void takeAction(){
+  if(isContain(stringVal,"start"))
+  {
+    Serial.println("This is start");
+  }
+   else if(isContain(stringVal,"stop"))
+  {
+    Serial.println("This is stop");
+  }
+   else if(isContain(stringVal,"checkstatus"))
+  {
+    Serial.println("This is checkstatus");
+    send_sms("Sabu bhala");
+  }
+   else if(isContain(stringVal,"fetchlocation"))
+  {
+    tracking();
+  }
+  return;
+}
+
 void recieveMessage()
 {
-    
     if (gsmSerial.available() > 0)
     {
-      char msg = gsmSerial.read();
-      Serial.print(msg);
-      if (msg==(char*)"start")
-      {
-        Serial.println(" this is start");
-      }
-    
-    
-      // Serial.write(gsmSerial.read());
-
-    //   if (gsmSerial.find((char*)"start"))
-    //   {
-    //     Serial.println("start");
-    //   }
-    //   else if (gsmSerial.find((char*)"stop"))
-    //   {
-    //     Serial.println("stop");
-    //   }
-    //   else if (gsmSerial.find((char*)"checkstatus"))
-    //   {
-    //     Serial.println("checkstatus");
-    //   }
-    //   else if (gsmSerial.find((char*)"fetchlocation"))
-    //   {
-    //     Serial.println("fetchlocation");
-    //   } else{
-    //     Serial.println("invalid");
-
-    //   }     
+      stringVal = gsmSerial.readString(); 
+      Serial.println("DATA Received");  
+      Serial.println(stringVal);  
+      delay(1000);
     }
-    
+    takeAction();
 }
 
 
-void tracking(){
-  init_sms();
-  send_data("Vehicle location is:");
-  gsmSerial.print("Latitude: ");
-  gsmSerial.print(gps.location.lat(), 6);
-  gsmSerial.print("\n Longitude: ");
-  gsmSerial.println(gps.location.lng(), 6);
-  send_sms();
-  init_receive_sms();
-}
+
 
 void loop() {
-  switch (gsmState)
-    {
-      case 's':
-        init_sms();
-        break;
-      case 'r':
-        init_receive_sms();
-        break;
-      default:
-      break;
-    }
-
-    recieveMessage();
-    
-
-  // while (temp)  //run the loop if temp==1;
-  // {
-  //   while (gpsSerial.available()>0)
-  //   {
-  //     gps.encode(gpsSerial.read());
-  //     if (gps.location.isUpdated())
-  //     {
-  //       temp=0;
-  //       tracking();
-  //     }
-  //   }
-  // }
+  recieveMessage();
 }
